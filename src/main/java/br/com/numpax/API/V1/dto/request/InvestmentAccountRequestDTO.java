f82d635a9b036d76f124536bab526c8cf5a85a38
Dias <guidiaspf@gmail.com>
2024-11-09 11:20:15 -0300

    fix

--- src/main/java/br/com/numpax/API/V1/dto/request/InvestmentAccountRequestDTO.java
+++ src/main/java/br/com/numpax/API/V1/dto/request/InvestmentAccountRequestDTO.java
@@ -3,8 +3,6 @@ package br.com.numpax.API.V1.dto.request;
 import br.com.numpax.application.enums.AccountType;
 import br.com.numpax.application.enums.InvestmentSubtype;
 import br.com.numpax.application.enums.RiskLevelType;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,18 +12,18 @@ import java.math.BigDecimal;
 @Setter
 public class InvestmentAccountRequestDTO extends AccountRequestDTO {
 
-//    private BigDecimal totalInvestedAmount;
-//    private BigDecimal totalProfit;
-//    private BigDecimal totalCurrentAmount;
-//    private BigDecimal totalWithdrawnAmount;
-//    private Integer numberOfWithdrawals;
-//    private Integer numberOfEntries;
-//    private Integer numberOfAssets;
-//    private BigDecimal averagePurchasePrice;
-//    private BigDecimal totalGainLoss;
-//    private BigDecimal totalDividendYield;
-//    private RiskLevelType riskLevelType;
-//    private InvestmentSubtype investmentSubtype;
+   private BigDecimal totalInvestedAmount;
+   private BigDecimal totalProfit;
+   private BigDecimal totalCurrentAmount;
+   private BigDecimal totalWithdrawnAmount;
+   private Integer numberOfWithdrawals;
+   private Integer numberOfEntries;
+   private Integer numberOfAssets;
+   private BigDecimal averagePurchasePrice;
+   private BigDecimal totalGainLoss;
+   private BigDecimal totalDividendYield;
+   private RiskLevelType riskLevelType;
+   private InvestmentSubtype investmentSubtype;
 
     public InvestmentAccountRequestDTO() {
         this.setAccountType(AccountType.INVESTMENT);
